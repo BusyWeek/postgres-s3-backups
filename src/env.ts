f2d770aa66d9fde7230d064fc4d13dc83c7033ac
Calum MacRae <calum.macrae@medayo.com>
2023-07-11 19:35:07 +0100

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -5,6 +5,10 @@ export const env = envsafe({
   AWS_SECRET_ACCESS_KEY: str(),
   AWS_S3_BUCKET: str(),
   AWS_S3_REGION: str(),
+  FOLDER: str({
+    desc: 'Where in the bucket the file is stored i.e. folder/another/ or /',
+    default: '/',
+  }),
   BACKUP_DATABASE_URL: str({
     desc: 'The connection string of the database to backup.'
   }),
